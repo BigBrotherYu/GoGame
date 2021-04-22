af235a1e7709f0f692d75757c7c12ac9f51882a7
Yu Xia <xiayudaxia@gmail.com>
2021-04-22 10:17:56 -0500

    functional update

--- game_engine.py
+++ game_engine.py
@@ -1,52 +1,280 @@
 import numpy as np
-import cv2
 
 
 class GoGame:
 
-    def __init__(self, game_size=19, handicap=None, rule='Zhongguo'):
-        assert game_size in [9, 13, 19]
-        assert rule in ['zhongguo', 'yingshi', 'hanguo', 'ribeng']
-        self.game_size = game_size - 1
+    def __init__(self, game_size=19, handicap=None, rule='zhongguo'):
+
+        # assert game_size in [9, 13, 19]
+        assert rule in ['zhongguo', 'yingshi', 'hanguo', 'ribeng', 'wuzi']
+
+        self.end_game = False
+        self.game_size = game_size
+
+        # game_board represent the current state of the game, 1 represent black stone, 2 represent white stone
         self.game_board = np.zeros(shape=(game_size, game_size), dtype=np.int8)
-        self.boardered =
+        # boardered is game_board with boarders, boarders are '3's
+        self.boardered = None
+        # map storing information of whether a stone have qi or not
+        self.qi_map = np.zeros(shape=(game_size, game_size), dtype=np.int8)
+        # group_map show which group each stone belows to
+        self.group_map = np.zeros(shape=(game_size, game_size), dtype=np.int8)
+        self.group_indexes = []
+        # da_jie is a boolean whether there is a jie
+        self.da_jie = False
+
         self.game_history = []
-        self.player = 1 # black goes
+        self.player = 1  # black goes
         self.rule = rule
         if handicap:
             self.handicap = []
-            self.player = 2 # white goes first when handicap
+            self.player = 2  # white goes first when handicap
 
     def add_boarder(self):
-        # this method give boader to the game_board matrix
-        vertical = np.ones(shape=(1, self.game_size), dtype=np.int8)
-        self.boardered = np.stack((3 * vertical, self.game_board, 3 * vertical), axis=1)
+        # this method give boader to the game_board matrix, which will generate the boadered map of all stones
+        vertical = np.ones(shape=(1, self.game_size), dtype=np.int8).T
+        self.boardered = np.column_stack((3 * vertical, self.game_board, 3 * vertical))
         horizontal = np.ones(shape=(1, self.game_size + 2))
-        self.boardered = np.stack((3 * horizontal, self.boardered, 3 * horizontal), axis=0)
-        return self.boardered
+        self.boardered = np.row_stack((3 * horizontal, self.boardered, 3 * horizontal))
 
     def switch_side(self):
         if self.player == 1:
-            self.player = 2
+            return 2
         if self.player == 2:
-            self.player = 1
+            return 1
 
     def place_stone(self, location: (int, int)):
         assert 0 <= location[0] < self.game_size
         assert 0 <= location[1] < self.game_size
         assert self.game_board[location[0]][location[1]] == 0
+        copy = self.game_board.copy()
         self.game_board[location[0]][location[1]] = self.player
-        self.switch_side()
-        self.game_history.append(self.game_board)
-        return self.game_board
 
-    def si_huo(self, last_play: (int, int)):
-        pass
+        if self.rule == 'wuzi':
+            step_result = self.wuziqi_judge(location)
+        else:
+            # 所有坐标加一， 把坐标换成有边缘的
+            self.add_boarder()
+            last_x = location[0]
+            last_y = location[1]
+
+            self.group_all_stones()
+            print('group map \n', self.group_map)
+            print('group index', self.group_indexes)
+            # 最后一子的所在组
+            last_group = self.group_map[last_x][last_y]
+
+            # 寻找那块棋死掉了
+            # self.update_qi_map()
+            # 更新qi_map
+            print('game_board', self.game_board)
+
+            self.qi_map[last_x][last_y] = self.qi(last_x, last_y)
+            if last_x > 0 and self.game_board[last_x - 1][last_y] != 0:
+                self.qi_map[last_x - 1][last_y] = self.qi(last_x - 1, last_y)
+                print('zuo', (last_x - 1, last_y))
+
+            if last_y > 0 and self.game_board[last_x][last_y - 1] != 0:
+                self.qi_map[last_x][last_y - 1] = self.qi(last_x, last_y - 1)
+                print('you', (last_x, last_y - 1))
+
+            if last_x < self.game_size - 1 and self.game_board[last_x + 1][last_y] != 0:
+                self.qi_map[last_x + 1][last_y] = self.qi(last_x + 1, last_y)
+                print('shang', (last_x + 1, last_y))
+
+            if last_y < self.game_size - 1 and self.game_board[last_x][last_y + 1] != 0:
+                self.qi_map[last_x][last_y + 1] = self.qi(last_x, last_y + 1)
+                print('xia', (last_x, last_y + 1))
+
+            print('qi map \n', self.qi_map)
+            result = self.qi_map * self.group_map
+            print('result map \n', result)
+            print(self.group_indexes)
+            for i in range(len(self.group_indexes)):
+                if self.group_indexes[i] not in result and self.group_indexes[i] != last_group:
+                    # 提子
+                    print('delete', self.group_indexes[i])
+                    self.del_group(self.group_indexes[i])
+                    self.group_indexes[i] = 0
+                    self.add_boarder()
+                    self.update_qi_map() # 更新全盘气的情况
+
+
+
+            # 查看是否入气
+            result = self.qi_map * self.group_map
+            if last_group not in result:
+                self.game_board[last_x][last_y] = 0
+                print('buruqi')
+                return self.game_board, 'Buruqi'  # 不入气返回原状
+            else:
+                step_result = False
+
+            # 查看是否打劫
+            if len(self.game_history) > 2 and np.array_equal(self.game_board, self.game_history[len(self.game_history)-2]):
+                self.game_board = copy
+                print('dajie')
+                return self.game_board, 'dajie'  # 不入气返回原状
+            else:
+                step_result = False
+
+        # print('appended', self.game_board)
+        game_status = self.game_board.copy()
+        self.game_history.append(game_status)
+        # print(self.game_history)
+        self.player = self.switch_side()
+        return self.game_board, step_result
+
+    def del_group(self, group):
+        mask = self.group_map - group
+        mask[mask != 0] = 1
+        self.game_board = self.game_board * mask
+
+
+    def update_qi_map(self):
+        print('new boardered', self.boardered)
+        for i in range(self.game_size):
+            for j in range(self.game_size):
+                if self.game_board[i][j]:
+                    self.qi_map[i][j] = self.qi(i, j)
+                else:
+                    self.qi_map[i][j] = 0
+        print('new_qi_map\n', self.qi_map)
+
+    def group_all_stones(self):
+        self.group_indexes = []
+        for i in range(self.game_size):
+            for j in range(self.game_size):
+                if self.game_board[i][j] == 0:
+                    self.boardered[i + 1][j + 1] = 0
+                elif self.game_board[i][j] == self.boardered[i][j+1]:
+                    self.group_map[i][j] = self.group_map[i - 1][j]
+                elif self.game_board[i][j] == self.boardered[i+1][j]:
+                    self.group_map[i][j] = self.group_map[i][j - 1]
+                else:
+                    index = len(self.group_indexes) + 1
+                    self.group_map[i][j] = index
+                    self.group_indexes.append(index)
 
     def qi(self, x, y):
         # 判断每个子有没有气， 有气返回1， 没气返回0
-        if any([self.boardered[x - 1][y], self.boardered[x + 1][y], self.boardered[x][y + 1], self.boardered[x][y - 1]]):
-            return 1
-        else:
+        ls = [self.boardered[x][y + 1], self.boardered[x + 1][y], self.boardered[x + 1][y + 2], self.boardered[x + 2][y + 1]]
+        if all(ls):
             return 0
+        else:
+            return 1
 
+    def loc_group(self, x, y):
+        # locate where each group
+        if self.boardered[x - 1][y] == self.player:
+            return self.group_map[x][y]
+        elif self.boardered[x][y - 1] == self.player:
+            return self.group_map[x][y]
+
+    def da_jie(self):
+        # this method monitering if there is a jie presented
+        pass
+
+    def wuziqi_judge(self, location):
+        # 判断使用五子棋规则下有没有赢
+        assert self.end_game is False
+
+        # determine if there are 5 connected stones horizontally
+        horizontal = 0
+        x, y = location
+        i, j = x, y
+        while i < self.game_size:
+            if self.game_board[i][j] == self.game_board[i + 1][j]:
+                horizontal += 1
+                i += 1
+            else:
+                break
+        i, j = x, y
+        while i >= self.game_size:
+            if self.game_board[i][j] == self.game_board[i - 1][j]:
+                horizontal += 1
+                i -= 1
+            else:
+                break
+
+        if horizontal >= 5:
+            self.end_game = True
+            return True
+
+        # determine if there are 5 connected stones vertically
+        vertical = 0
+        x, y = location
+        i, j = x, y
+        while j < self.game_size:
+            if self.game_board[i][j] == self.game_board[i][j + 1]:
+                vertical += 1
+                j += 1
+            else:
+                break
+        i, j = x, y
+        while j >= self.game_size:
+            if self.game_board[i][j] == self.game_board[i][j - 1]:
+                vertical += 1
+                j -= 1
+            else:
+                break
+
+        if vertical >= 5:
+            self.end_game = True
+            return True
+
+        # determine if there are 5 connected stones diagonally from left to right
+        diagonal_left = 0
+        x, y = location
+        i, j = x, y
+        while i < self.game_size and j < self.game_size:
+            if self.game_board[i][j] == self.game_board[i + 1][j + 1]:
+                diagonal_left += 1
+                i += 1
+                j += 1
+            else:
+                break
+        while i >= self.game_size and j >= self.game_size:
+            if self.game_board[i][j] == self.game_board[i - 1][j - 1]:
+                diagonal_left += 1
+                i -= 1
+                j -= 1
+            else:
+                break
+
+        if diagonal_left >= 5:
+            self.end_game = True
+            return True
+
+        # determine if there are 5 connected stones diagonally from right to left
+        diagonal_right = 0
+        x, y = location
+        i, j = x, y
+        while i >= self.game_size and j < self.game_size:
+            if self.game_board[i][j] == self.game_board[i - 1][j + 1]:
+                diagonal_right += 1
+                i -= 1
+                j += 1
+            else:
+                break
+        i, j = x, y
+        while i < self.game_size and j >= self.game_size:
+            if self.game_board[i][j] == self.game_board[i + 1][j - 1]:
+                diagonal_right += 1
+                i += 1
+                j -= 1
+            else:
+                break
+
+        if diagonal_right >= 5:
+            self.end_game = True
+            return 'end_game'
+
+    def revert(self):
+        # revert the game play to the last state
+        self.game_board = self.game_history[len(self.game_history) - 1]
+        del self.game_history[len(self.game_history)]
+        self.switch_side()
+
+    def Zhong_guo(self):
+        pass
--- gameboard.py
+++ gameboard.py
@@ -3,6 +3,7 @@ import time
 import sys
 from pygame.locals import *
 import numpy as np
+from game_engine import GoGame
 
 
 global initChessList, resultFlag
@@ -14,7 +15,7 @@ game_status = np.zeros((19, 19), dtype=np.int8)
 
 
 class StonePoint():
-    def __init__(self,x,y,value):
+    def __init__(self,x,y):
         '''
         :param x: 代表x轴坐标
         :param y: 代表y轴坐标
@@ -22,7 +23,6 @@ class StonePoint():
         '''
         self.x = x            #初始化成员变量
         self.y = y
-        self.value = value
 
 def initChessSquare(x,y):     #初始化棋盘
     for i in range(15):       # 每一行的交叉点坐标
@@ -30,7 +30,7 @@ def initChessSquare(x,y):     #初始化棋盘
         for j in range(15):   # 每一列的交叉点坐标
             pointX = x+ j*40
             pointY = y+ i*40
-            sp = StonePoint(pointX,pointY,0)
+            sp = StonePoint(pointX,pointY)
             rowlist.append(sp)
         initChessList.append(rowlist)
 
@@ -49,65 +49,18 @@ def eventHander():            #监听各种事件
                     if x>=point.x-10 and x<=point.x+10 and y>=point.y-10 and y<=point.y+10:
                         if point.value == 0 and player == 1:   #当棋盘位置为空；棋子类型为白棋
                             point.value = 1             #鼠标点击时，棋子为白棋
-                            judgeResult(i,j,1)
+                            # judgeResult(i,j,1)
                             player = 2                #切换角色
                         elif point.value == 0 and player ==2:  #当棋盘位置为空；棋子类型为黑棋
                             point.value = 2             #鼠标点击时，棋子为黑棋
-                            judgeResult(i,j,2)
+                            # judgeResult(i,j,2)
                             player = 1                #切换角色
                         break
-                    j+=1
-                i+=1
-                j=0
+                    j += 1
+                i += 1
+                j = 0
 
-def judgeResult(i,j,value):   #横向判断
-    global resultFlag
-    flag = False
-    for  x in  range(j - 4, j + 5):  # 横向有没有出现5连（在边缘依次逐一遍历，是否五个棋子的类型一样）
-        if x >= 0 and x + 4 < 15 :
-            if initChessList[i][x].value == value and \
-                initChessList[i][x + 1].value == value and \
-                initChessList[i][x + 2].value == value and \
-                initChessList[i][x + 3].value == value and \
-                initChessList[i][x + 4].value == value :
-                flag = True
-                break
-                pass
-    for x in range(i - 4, i + 5):  # 纵向有没有出现5连（在边缘依次逐一遍历，是否五个棋子的类型一样）
-        if x >= 0 and x + 4 < 15:
-            if initChessList[x][j].value == value and \
-                    initChessList[x + 1][j].value == value and \
-                    initChessList[x + 2][j].value == value and \
-                    initChessList[x + 3][j].value == value and \
-                    initChessList[x + 4][j].value == value:
-                flag = True
-                break
-                pass
-
-    # 先判断东北方向的对角下输赢 x 列轴， y是行轴 ， i 是行 j 是列（右斜向）（在边缘依次逐一遍历，是否五个棋子的类型一样）
-    for x, y in zip(range(j + 4, j - 5, -1), range(i - 4, i + 5)):
-        if x >= 0 and x + 4 < 15 and y + 4 >= 0 and y < 15:
-            if initChessList[y][x].value == value and \
-                    initChessList[y - 1][x + 1].value == value and \
-                    initChessList[y - 2][x + 2].value == value and \
-                    initChessList[y - 3][x + 3].value == value and \
-                    initChessList[y - 4][x + 4].value == value:
-                flag = True
-
-    # 2、判断西北方向的对角下输赢 x 列轴， y是行轴 ， i 是行 j 是列（左斜向）（在边缘依次逐一遍历，是否五个棋子的类型一样）
-    for x, y in zip(range(j - 4, j + 5), range(i - 4, i + 5)):
-        if x >= 0 and x + 4 < 15 and y >= 0 and y + 4 < 15:
-            if initChessList[y][x].value == value and \
-                    initChessList[y + 1][x + 1].value == value and \
-                    initChessList[y + 2][x + 2].value == value and \
-                    initChessList[y + 3][x + 3].value == value and \
-                    initChessList[y + 4][x + 4].value == value:
-                flag = True
-
-
-    if flag:               #如果条件成立，证明五子连珠
-        resultFlag = value #获取成立的棋子颜色
-        print("白棋赢" if value == 1 else "黑棋赢")
+resultFlag = 0
 
 if __name__ == '__main__':
     # 加载素材
@@ -121,15 +74,22 @@ if __name__ == '__main__':
     blackStone = pygame.image.load("stone_black.png")  # 加载黑棋图片
     resultStone = pygame.image.load("result.jpg")  # 加载 赢 时的图片
     rect = blackStone.get_rect()
+    game1 = GoGame(game_size=15)
 
     while True:
         screen.blit(background, (0, 0))
+        i = 0
+        j = 0
         for temp in initChessList:
             for point in temp:
-                if point.value == 1:  # 当棋子类型为1时，绘制白棋
+                val = game1.game_board[i][j]
+                if val == 1:
                     screen.blit(blackStone, (point.x - 18, point.y - 18))
-                elif point.value == 2:  # 当棋子类型为2时，绘制黑棋
+                elif val == 2:
                     screen.blit(whiteStone, (point.x - 18, point.y - 18))
+                j += 1
+            i += 1
+            j = 0
 
         if resultFlag > 0:
             initChessList = []  # 清空棋盘
@@ -140,5 +100,21 @@ if __name__ == '__main__':
         if resultFlag > 0:
             time.sleep(3)
             resultFlag = 0  # 置空之前的获胜结果
-        eventHander()  # 调用之前定义的事件函数
 
+        for event in pygame.event.get():
+            if event.type == QUIT:  # 事件类型为退出时
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:  # 当点击鼠标时
+                x, y = pygame.mouse.get_pos()  # 获取点击鼠标的位置坐标
+                i = 0
+                j = 0
+                for temp in initChessList:
+                    for point in temp:
+                        if x >= point.x - 10 and x <= point.x + 10 and y >= point.y - 10 and y <= point.y + 10:
+                            if game1.game_board[i][j] == 0:
+                                gameboard, result = game1.place_stone((i, j))
+                            break
+                        j += 1
+                    i += 1
+                    j = 0
